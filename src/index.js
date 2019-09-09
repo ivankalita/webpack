f9f76315aa11a5a463f51649995a2f5a911951ad
Ivan K <ivan_kalita90@mail.ru>
2019-09-09 19:32:20 +0300

    fisrt config

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Varinats connect libraries
+ * 
+ * import 'vue'
+ * import 'bootstrap/dist/css/bootstrap.min.css'
+ * import Vue from 'vue'
+ * window.Vue = require('vue')
+ */
+
+import './js/common'
+import './styl/main.styl'
+import 'bootstrap/dist/css/bootstrap.min.css'
